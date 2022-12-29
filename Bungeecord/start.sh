6b27200647f889ecd15f77dca74f94b1dab455c8
DiffuseHyperion <95083897+DiffuseHyperion@users.noreply.github.com>
2022-12-29 13:14:09 +0800

    Added auto eaglercraftX plugin download

--- Bungeecord/start.sh
+++ Bungeecord/start.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 DOWNLOAD_URL=https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/bootstrap/target/BungeeCord.jar
+PLUGIN_DOWNLOAD_URL=https://gitlab.com/lax1dude/eaglercraftx-1.8/-/raw/main/gateway/EaglercraftXBungee/EaglerXBungee-Latest.jar
 
 cd /server || exit
 
 wget -O server.jar ${DOWNLOAD_URL}
+
+mkdir plugins
+cd plugins
+wget ${PLUGIN_DOWNLOAD_URL}
+cd ..
 
 java -Xms${MIN_RAM} -Xmx${MAX_RAM} -jar server.jar
